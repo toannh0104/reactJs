8de145091043af3c1cecd4922c65507987bceb05
ToanNH <toannh0401@gmail.com>
2016-10-24 18:02:54 +0700

    111

---
+++ src/data/posts.js
@@ -0,0 +1,38 @@
+
+const posts = [
+   {
+      "code":"BAcyDyQwcXX",
+      "caption":"Lunch #hamont",
+      "likes":56,
+      "id":"1161022966406956503",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12552326_495932673919321_1443393332_n.jpg"
+   },
+   {
+      "code":"BAcJeJrQca9",
+      "caption":"Snow! ⛄️🌨❄️ #lifewithsnickers",
+      "likes":59,
+      "id":"1160844458347054781",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg"
+   },
+   {
+      "code":"BAF_KY4wcRY",
+      "caption":"Cleaned my office and mounted my recording gear overhead. Stoked for 2016!",
+      "likes":79,
+      "id":"1154606670337393752",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e35/923995_1704188643150533_1383710275_n.jpg"
+   }
+];
+
+export default posts;
+// var xx = fetchData();
+// console.log(xx);
+// export const GET_PHOTO = 'GET_PHOTO';
+//
+// function fetchData() {
+//    return (dispatch) => fetch('/api/photos')
+//        .then(res => res.json())
+//        .then(json => dispatch({
+//           type: 'fetchData',
+//           data: json
+//        }));
+// }
---
+++ src/reducers/photos.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Toan_H on 10/5/2016.
+ */
+// const initialState = {
+// 	fetching: false,
+// 	fetched: false,
+// 	photos: [],
+// 	error: null
+// }
+//
+// function photos(state = initialState, action) {
+//     switch(action.type) {
+//         case 'FETCH_PHOTOS' :
+//             console.log("fetch photos !!!");
+// 			return{...state, photos: action.payload}
+// 			break;
+//         default:
+//             return state;
+//     }
+// }
+//
+// export default photos;
